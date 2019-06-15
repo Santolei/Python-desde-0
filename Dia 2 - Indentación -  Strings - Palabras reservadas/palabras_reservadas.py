0dcf17ee3891f4b7cb1837a63e82847ad9e82aaa
Santiago <s.tolosa1@gmail.com>
2019-06-15 17:19:42 -0300

    Día 2 de aprender python

---
+++ Dia 2 - Indentación -  Strings - Palabras reservadas/palabras_reservadas.py
@@ -0,0 +1,21 @@
+# Como en todos los lenguajes de programación, existen ciertas palabras reservadas que no podemos utilizar para nombrar
+# variables. Son las siguientes:
+
+# False               def                 if                  raise
+# None                del                 import              return
+# True                elif                in                  try
+# and                 else                is                  while
+# as                  except              lambda              with
+# assert              finally             nonlocal            yield
+# break               for                 not
+# class               from                or
+# continue            global              pass
+
+# También podemos ver este listado desde la consola de Python tipeando help() y luego "keywords"
+
+# Veamos que pasa si las usamos:
+
+for = 4
+
+# Si ejecutamos en consola nos va a marcar un error:
+# SyntaxError: invalid syntax
